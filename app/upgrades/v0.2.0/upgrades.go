054d9a64eb3f0878309a09dc7498a54b25f7eab2
mbreithecker <m@breithecker.de>
2022-04-19 17:25:27 +0200

    fix: handle overflow error during upgrade

--- app/upgrades/v0.2.0/upgrades.go
+++ app/upgrades/v0.2.0/upgrades.go
@@ -41,6 +41,39 @@ func CreateUpgradeHandler(
 			}
 		}
 
+		// Fix overflow in state of user kyve13xzj5e568n4kwe76ayzmzzuraz6c9vnaslrn3t and restore integrity
+
+		// What happened?
+		// During the unbonding-period the user got kicked out of the stakers-list
+		// leading to the deletion of the stakers-entry. A short time after that, the user
+		// rejoined the stakers list. Now the staker.UnbondingAmount was zero !!
+		// This value then experienced an integer-overflow which then in a next step
+		// allowed for the actual overflow in the staker amount.
+
+		// The stake on block #153457 was 18_026_400_000_000 tkyve
+		// On block #153458 the overflow occurred by unstaking 19_312_000_000_000 tkyve
+
+		// Transfer original funds back ...
+		registryKeeper.TransferToAddress(ctx, "kyve13xzj5e568n4kwe76ayzmzzuraz6c9vnaslrn3t", 18_026_400_000_000)
+		// ... and delete staker entry and remove it from the pool
+		registryKeeper.RemoveStaker(ctx, "kyve13xzj5e568n4kwe76ayzmzzuraz6c9vnaslrn3t", 1)
+
+		pool, _ := registryKeeper.GetPool(ctx, 1)
+
+		var stakerIndex = -1
+		for i, v := range pool.Stakers {
+			if v == "kyve13xzj5e568n4kwe76ayzmzzuraz6c9vnaslrn3t" {
+				stakerIndex = i
+				break
+			}
+		}
+
+		// Remove staker from list of stakers (replace with last entry and then slice).
+		pool.Stakers[stakerIndex] = pool.Stakers[len(pool.Stakers)-1]
+		pool.Stakers = pool.Stakers[:len(pool.Stakers)-1]
+
+		registryKeeper.SetPool(ctx, pool)
+
 		// Return.
 		return vm, nil
 	}
